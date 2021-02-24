0d9d9d25edc929fb9f1a8ff93addcc0463cccfa2
Jack Curtin <jcurtin4@binghamton.edu>
2021-02-24 10:55:58 -0500

    Working functions, Building out game now

--- Multiscreen/Multiscreen/ViewController.h
+++ Multiscreen/Multiscreen/ViewController.h
@@ -8,6 +8,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (nonatomic, strong) IBOutlet UIButton *winButton;
+@property (nonatomic, strong) IBOutlet UIButton *loseButton;
+
+@property (nonatomic, strong) IBOutlet UILabel *r1_l;
+@property (nonatomic, strong) IBOutlet UILabel *r1_r;
+
 
 
 @end
--- Multiscreen/Multiscreen/ViewController.m
+++ Multiscreen/Multiscreen/ViewController.m
@@ -12,10 +12,52 @@
 @end
 
 @implementation ViewController
+@synthesize r1_l;
+@synthesize r1_r;
+
+@synthesize winButton;
+@synthesize loseButton;
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    winButton.hidden = YES;
+    loseButton.hidden = YES;
+
+
+}
+- (IBAction)fun_r1_l:(id)sender
+{
+    if([r1_l.text isEqualToString:@"You explore the current room"]){
+        [r1_l setText:@"You see the exit, Go there"];
+        [r1_r setText:@"Something shiny...? Go there"];
+        return;
+    }
+    if([r1_l.text isEqualToString:@"You see the exit, Go there"]){
+        [r1_l setText:@"Escape!"];
+        [r1_r setText:@" "];
+        winButton.hidden = NO;
+        return;
+    }
+}
+
+- (IBAction)fun_r1_r:(id)sender
+{
+    if([r1_r.text isEqualToString:@"You walk down into the smaller cavern"]){
+        [r1_l setText:@"A fork, Go left"];
+        [r1_r setText:@"A fork, Go right"];
+        return;
+    }
+    
+    if([r1_r.text isEqualToString:@"Something shiny...? Go there"]){
+        [r1_r setText:@"It's a bear trap"];
+        [r1_l setText:@""];
+        loseButton.hidden = NO;
+        return;
+    }
 }
 
 
